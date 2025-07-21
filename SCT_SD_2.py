788b070cf25e876a505e34ef080091a647fb4cce
arjunrvk2502 <137705486+arjunrvk2502@users.noreply.github.com>
2025-07-21 15:19:35 +0530

    Add files via upload

---
+++ SCT_SD_2.py
@@ -0,0 +1,35 @@
+import numpy as np
+
+def number_guessing(r):
+    random_number = np.random.randint(r+1)
+    attempts = 0
+    guess = None
+    i = 0 #lower bound
+    j = r #upper bound
+    print("Welcome to the Number Guessing Game!")
+    print(f"I'm thinking of a number between {i} and {j}.")
+    while guess != random_number:
+        try:
+            guess = int(input(f"Enter a between number({i}-{j}): "))
+            if guess > random_number:
+                print("High")
+                if j > guess:
+                    j = guess
+            elif guess < random_number:
+                print("Low")
+                if i < guess:
+                    i = guess
+            attempts += 1
+            
+        except ValueError:
+            print("Invalid input. Please enter a number.")
+
+    print(f"Congratulations! You guessed the number {guess} correctly! In {attempts} Attempts")
+if __name__ == "__main__":
+    while True:
+        try:
+            length = int(input("Enter a Upper Limit for the Number Guessing Game (Ex: 100 , 500 , 1000):\n"))
+            number_guessing(length)
+        except:
+            print("Invalid input. Plese enter a number\n")
+    
